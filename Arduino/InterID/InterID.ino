2b56e12fd26bddda04d87102a62dcbd208630724
NoahRAlmeda <noahalmeda@gmail.com>
2019-09-16 18:38:52 -0400

    Initial commit

---
+++ Arduino/InterID/InterID.ino
@@ -0,0 +1,60 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_1_PIN        10         // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 2
+#define SS_2_PIN        8          // Configurable, take a unused pin, only HIGH/LOW required, must be diffrent to SS 1
+
+#define NR_OF_READERS   2
+
+byte ssPins[] = {SS_1_PIN, SS_2_PIN};
+
+MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
+
+const int buzzer = 3;
+
+/**
+ * Initialize.
+ */
+void setup() {
+
+  Serial.begin(9600); // Initialize serial communications with the PC
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+
+  SPI.begin();        // Init SPI bus
+
+  pinMode(buzzer, OUTPUT);
+  
+}
+
+/**
+ * Main loop.
+ */
+void loop() {
+  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+    // Look for new cards
+
+    if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
+      tone(buzzer, 1000);
+      delay(500);
+      noTone(buzzer);
+      dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
+      Serial.println();
+
+      // Halt PICC
+      mfrc522[reader].PICC_HaltA();
+      // Stop encryption on PCD
+      mfrc522[reader].PCD_StopCrypto1();
+    } //if (mfrc522[reader].PICC_IsNewC
+  } //for(uint8_t reader
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial.
+ */
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
---
+++ sheets.py
@@ -0,0 +1,76 @@
+# TODO
+# - Fix Bug - If the person has never entered in the spreadsheet, app will crash because it can't find the cell of a non existed data entry
+# - Comment code
+# -
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import serial
+import pandas as pd
+from datetime import datetime
+import time
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive']
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open('CoReLabCheckInList').sheet1
+
+ser = serial.Serial('/dev/cu.usbmodem141201', 9600)  # open serial port
+
+file = 'Book1.csv'
+df = pd.read_csv(file)
+
+
+def scanCard():
+    global ser
+    while(True):
+        if(ser.inWaiting() > 0):
+            print("READING DATA")
+            return ser.readline().decode('UTF-8').strip()
+
+def findCell(n, i, t, d):
+    try:
+        return (sheet.find(n), False)
+    except gspread.exceptions.CellNotFound:
+        addEntry(n, i, t, d)
+        return (sheet.find(n), True)
+
+def addEntry(n, i, t, d):
+    sheet.insert_row([n, i, t, d, '', 'False'], 2)
+    print('Checked In')
+
+def getRowValue(row):
+    return sheet.row_values(row)
+
+def checkOut(cell):
+    outTimeCell = 'E' + str(cell.row)
+    switchToTrue = 'F' + str(cell.row)
+    sheet.update_acell(outTimeCell, str(datetime.now().strftime('%H:%M:%S')))
+    sheet.update_acell(switchToTrue, 'True')
+
+def checkIn():
+    print("WAITING...")
+    cardId = scanCard()
+    print("CARDID: ", cardId)
+    name = df[df['Id'] == cardId]
+    n = name['name'].values[0]
+    i = name['studentId'].values[0]
+    t = datetime.now().strftime('%H:%M:%S')
+    d = datetime.now().strftime('%Y-%m-%d')
+    cell, can_read = findCell(n, i, t, d)
+    values_list = getRowValue(cell.row)
+    print(values_list)
+
+    if not can_read and (values_list[5] == 'False'):
+        print('Not checked out')
+        checkOut(cell)
+    else:
+        print('Checked In')
+        addEntry(n, i, t, d)
+
+
+while True:
+    checkIn()
